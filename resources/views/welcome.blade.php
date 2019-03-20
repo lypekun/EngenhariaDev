d1a1cc43a5ca01714bac9e81b3f6b7f6f14fb0ac
lypekun <lype.remake@gmail.com>
2019-03-19 22:29:12 -0300

    Atualizando pagina index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,26 +72,20 @@
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
+                        <!-- @if (Route::has('register'))
+                          <a href="{{ route('register') }}">Register</a>
+                        @endif -->
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema de Login
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="m-b-md">
+                    <p>Trabalho desenvolvido para a disciplina de Engenharia de Software II</p>
                 </div>
             </div>
         </div>
